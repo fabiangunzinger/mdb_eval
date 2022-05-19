c8dd8f96e777bdc6112af2984fcf7e5ce9050340
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-05-19 16:51:43 +0100

    Large files

--- src/data/make_data.py
+++ src/data/make_data.py
@@ -50,8 +50,8 @@ def clean_piece(filepath):
 
 
 def get_filepath(piece):
-    # return os.path.join(config.AWS_PIECES, f"mdb_XX{piece}.parquet")
-    return f's3://3di-project-eval/eval_XX{piece}.parquet'
+    return os.path.join(config.AWS_PIECES, f"mdb_XX{piece}.parquet")
+    # return f's3://3di-project-eval/eval_XX{piece}.parquet'
 
 
 def parse_args(args):
