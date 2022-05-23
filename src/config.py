9970bded8305992f1832b49c17d22d281a324609
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-05-23 11:23:36 +0100

    Validators and transformers

--- src/config.py
+++ src/config.py
@@ -14,3 +14,10 @@ ROOTDIR = Path(__file__).parent.parent
 FIGDIR = os.path.join(ROOTDIR, "output", "figures")
 TABDIR = os.path.join(ROOTDIR, "output", "tables")
 
+
+# Sample selection parameters
+MIN_INCOME = 5_000
+MIN_TOTAL_MONTHS = 12
+MIN_PRE_MONTHS = 6
+MIN_MONTH_SPEND = 200
+
---
+++ src/data/transformers.py
@@ -0,0 +1,42 @@
+"""
+Functions to transform variables.
+
+"""
+
+import src.helpers.data as hd
+
+
+transformer_funcs = []
+
+
+def transformer(func):
+    """Add func to list of transformer functions."""
+    transformer_funcs.append(func)
+    return func
+
+
+@transformer
+def winsorise_sa_flows(df):
+    df['inflows'] = hd.winsorise(df.inflows, pct=1, how='upper')
+    df['outflows'] = hd.winsorise(df.outflows, pct=1, how='upper')
+    df['netflows'] = hd.winsorise(df.netflows, pct=1, how='both')
+    df['inflows_norm'] = hd.winsorise(df.inflows_norm, pct=1, how='upper')
+    df['outflows_norm'] = hd.winsorise(df.outflows_norm, pct=1, how='upper')
+    df['netflows_norm'] = hd.winsorise(df.netflows_norm, pct=1, how='both')
+    return df
+
+
+@transformer
+def winsorise_month_income(df):
+    df['month_income'] = hd.winsorise(df.month_income, pct=1, how='upper')
+    return df
+
+
+@transformer
+def winsorise_month_spend(df):
+    df['month_spend'] = hd.winsorise(df.month_spend, pct=1, how='upper')
+    return df
+
+
+
+
