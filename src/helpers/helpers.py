3f1c3de67718816c556696b66d17f07020344948
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-05-23 11:53:02 +0100

    Timer with decorator factory
    Allows for easy switch on and off

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -2,19 +2,22 @@ import time
 import functools
 
 
-def timer(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        diff = end - start
-        unit = 'seconds'
-        if diff > 60:
-            diff = diff / 60
-            unit = 'minutes'
-        print(f'Time for {func.__name__:30}: {diff:.2f} {unit}')
-        return result
-    return wrapper
+def timer(func=None, active=True):
+    def time(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            if active:
+                diff = end - start
+                unit = "seconds"
+                if diff > 60:
+                    diff /= 60
+                    unit = "minutes"
+                    print(f"Time for {func.__name__:30}: {diff:.2f} {unit}")
+            return result
 
+        return wrapper
 
+    return timer(func) if func else timer
