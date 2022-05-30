9561f48461dc7d4aa2143a7d0312b29c940de0c7
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-05-30 11:54:47 +0100

    Reg results

--- src/.Rprofile
+++ src/.Rprofile
@@ -14,3 +14,6 @@ TABDIR <- file.path(ROOT, 'output/tables')
 # https://github.com/karthik/wesanderson/blob/master/R/colors.R
 treat_col <- '#78B7C5'
 untreat_col <- '#E1AF00'
+
+
+
--- src/analysis/regressions.R
+++ src/analysis/regressions.R
@@ -1,52 +1,89 @@
 library(dplyr)
-library(ggplot2)
-library(gridExtra)
 library(fixest)
+library(gridExtra)
+library(lubridate)
 
 source('./helpers/helpers.R')
 source('./helpers/fixest_settings.R')
 
-df <- read_s3parquet('s3://3di-project-eval/eval.parquet')
-df <- df %>%
-  filter(between(tt, -6, 6)) %>%
-  mutate(treat=1)
-
+df <- read_analysis_data() %>%
+  filter(between(tt, -6, 12)) %>%
+  mutate(treat = 1, month=month(ym(ymn), label = T))
 
 
 setFixest_fml(
-  ..endog = ~netflows_norm,
-  ..exog = ~t,
-  ..controls = ~month_income + month_spend + is_female + is_urban + i(generation),
-  ..fe = ~user_id + ym
+  ..controls = ~month_income + month_spend + disc_spend + is_female + i(generation, "Boomers"),
+  ..fe = ~user_id + month
 )
 
-# Simple pre-post comparison
+fcoefplot <- function(model, ...) {
+  coefplot(
+    model,
+    lab.fit = "tilted",
+    ylab = 'Change in __depvar__',
+    main = "",
+    col = c(1, 2, 4, 3, 5, 6, 7, 8),
+    pt.pch = c(20, 15, 17, 21, 24, 22),
+    ...
+  )
+}
 
-est <- feols(netflows ~ t, df)
-coefplot(est)
-etable(est)
+fiplot <- function(model, ...) {
+  iplot(
+    model,
+    ylab = 'Change in __depvar__',
+    main = "",
+    col = c(1, 2, 4, 3, 5, 6, 7, 8),
+    pt.pch = c(20, 15, 17, 21, 24, 22),
+    ...
+  )
+  
+}
 
-est <- feols(netflows ~ i(tt, treat, 0), data=data)
-est <- feols(netflows ~ i(tt), data=data)
 
-png("~/test.png")
-iplot(est)
+# Naive pre-post comparison
+figname <- 'reg_naive_pre_post.png'
+m_stat_nv <- feols(netflows ~ t, df)
+m_dynam_nv <- feols(netflows ~ i(tt, 0), df)
+etable(m_stat_nv, m_dynam_nv)
+figure(figname)
+par(mfrow=c(1, 2))
+fiplot(m_dynam_nv)
+fcoefplot(m_stat_nv)
 dev.off()
 
+# Adding controls
+figname <- 'reg_controls.png'
+m_stat_ctrl <- feols(netflows ~ t + ..controls, df)
+m_dynam_ctrl <- feols(netflows ~ i(tt, 0) + ..controls, df)
+etable(m_stat_ctrl, m_dynam_ctrl)
+figure(figname)
+par(mfrow=c(1, 2))
+fiplot(m_dynam_ctrl)
+fcoefplot(m_stat_ctrl)
+dev.off()
 
+# Adding FEs
+figname <- 'reg_twfe.png'
+m_stat_twfe <- feols(netflows ~ t + ..controls | ..fe, df)
+m_dynam_twfe <- feols(netflows ~ i(tt, 0) + ..controls | ..fe, df)
+etable(m_stat_twfe, m_dynam_twfe)
+figure(figname)
+par(mfrow=c(1, 2))
+fiplot(m_dynam_twfe)
+fcoefplot(m_stat_twfe)
+dev.off()
 
-etable(
-  est
-  # title = 'Components exploration'
-  # order = c('[Ee]ntropy', 'Average', 'Cnz', 'Counts std')
-  # drop = c('spend_')
-  # ,
-  # notes = c(note),
-  # tex = T,
-  # fontsize = 'tiny',
-  # file=file.path(TABDIR, glue('reg_has_sa_inflows_explore.tex'))
-  # label = glue('tab:reg_has_sa_inflows_explore'),
-  # replace = T
-)
+# Comparison plots
+figname <- "reg_comparison.png"
+figure(figname, height=2000, width=2000)
+par(mfrow=c(2, 1))
+fiplot(list(m_dynam_nv, m_dynam_ctrl, m_dynam_twfe))
+legend("topleft", col = c(1, 2, 4), pch = c(20, 15, 17), 
+       legend = c("Naive", "Controls", "TWFE"))
+fcoefplot(list(m_stat_nv, m_stat_ctrl, m_stat_twfe))
+legend("topleft", col = c(1, 2, 4), pch = c(20, 15, 17), 
+       legend = c("Naive", "Controls", "TWFE"))
 
+dev.off()
 
