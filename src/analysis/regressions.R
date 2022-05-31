fd9de38a8d08003f8eb6edcbaba0cc61bd55bdc3
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-05-31 13:02:04 +0100

    Reg updates

--- src/analysis/regressions.R
+++ src/analysis/regressions.R
@@ -7,13 +7,11 @@ source('./helpers/helpers.R')
 source('./helpers/fixest_settings.R')
 
 df <- read_analysis_data() %>%
-  filter(between(tt, -6, 12)) %>%
-  mutate(treat = 1, month=month(ym(ymn), label = T))
+  filter(between(tt, -6, 12))
 
 
 setFixest_fml(
-  ..controls = ~month_income + month_spend + disc_spend + is_female + i(generation, "Boomers"),
-  # + region_code + num_accounts,
+  ..controls = ~month_income + month_spend + discret_spend + is_female + i(generation, "Boomers") + region_code + num_accounts,
   ..fe = ~user_id + month,
   ..mvfe = ~mvsw(user_id, month)
 )
@@ -46,16 +44,25 @@ figname <- "reg_comparison.png"
 figure(figname, height=2000, width=2000)
 par(mfrow=c(2, 1))
 fiplot(list(m_dynam_pp, m_dynam_twfe))
-legend("topleft", col = c(1, 2, 4), pch = c(20, 15, 17), 
-       legend = c("Pre-post", "Controls", "TWFE"))
+# legend("topleft", col = c(1, 2, 4), pch = c(20, 15, 17), 
+       # legend = c("Pre-post", "TWFE"))
 fcoefplot(list(m_stat_pp, m_stat_twfe), keep="App use")
-legend("topleft", col = c(1, 2, 4), pch = c(20, 15, 17), 
-       legend = c("Pre-post", "Controls", "TWFE"))
+# legend("topleft", col = c(1, 2, 4), pch = c(20, 15, 17), 
+       # legend = c("Pre-post", "TWFE"))
 dev.off()
 
-# Internal vs external margin
-m_dynam_twfe_int <- feols(pos_netflows ~ i(tt, 0) + ..controls | ..mvfe, df)
-m_dynam_twfe_ext <- feols(has_pos_netflows ~ i(tt, 0) + ..controls | ..mvfe, df)
+# Decompose net-inflows
+m_dynam_twfe_net <- feols(netflows ~ i(tt, 0) + ..controls | ..fe, df)
+m_dynam_twfe_in <- feols(inflows ~ i(tt, 0) + ..controls | ..fe, df)
+m_dynam_twfe_out <- feols(outflows ~ i(tt, 0) + ..controls | ..fe, df)
+fiplot(list(m_dynam_twfe_net, m_dynam_twfe_in, m_dynam_twfe_out))
+
+# Decompose internal vs external margin
+m_dynam_twfe_int <- feols(pos_netflows ~ i(tt, 0) + ..controls | ..fe, df)
+m_dynam_twfe_ext <- feols(has_pos_netflows ~ i(tt, 0) + ..controls | ..fe, df)
+fiplot(m_dynam_twfe_int)
+fiplot(m_dynam_twfe_ext)
+
 
 # Tables
 tabname <- "reg_compare.tex"
