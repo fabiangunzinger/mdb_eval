41656b1d8dc73bc654606879a37b1272c62da24d
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-06-02 15:02:12 +0100

    Winsorise at 1 pct level

--- src/data/transformers.py
+++ src/data/transformers.py
@@ -3,13 +3,14 @@ Functions to transform variables.
 
 """
 
+import scr.config as config
 import src.helpers.data as hd
 
 
 transformers = []
 
 
-WIN_PCT = 5
+WIN_PCT = 1
 
 
 def transformer(func):
@@ -20,18 +21,26 @@ def transformer(func):
 
 @transformer
 def winsorise(df):
-    df["inflows"] = hd.winsorise(df.inflows, pct=WIN_PCT, how="upper")
-    df["outflows"] = hd.winsorise(df.outflows, pct=WIN_PCT, how="upper")
-    df["netflows"] = hd.winsorise(df.netflows, pct=WIN_PCT / 2, how="both")
-    df["pos_netflows"] = hd.winsorise(df.pos_netflows, pct=WIN_PCT / 2, how="upper")
-    df["inflows_norm"] = hd.winsorise(df.inflows_norm, pct=WIN_PCT, how="upper")
-    df["outflows_norm"] = hd.winsorise(df.outflows_norm, pct=WIN_PCT, how="upper")
-    df["netflows_norm"] = hd.winsorise(df.netflows_norm, pct=WIN_PCT / 2, how="both")
+    df["inflows"] = hd.winsorise(df.inflows, pct=config.WIN_PCT, how="upper")
+    df["outflows"] = hd.winsorise(df.outflows, pct=config.WIN_PCT, how="upper")
+    df["netflows"] = hd.winsorise(df.netflows, pct=config.WIN_PCT / 2, how="both")
+    df["pos_netflows"] = hd.winsorise(
+        df.pos_netflows, pct=config.WIN_PCT / 2, how="upper"
+    )
+    df["inflows_norm"] = hd.winsorise(df.inflows_norm, pct=config.WIN_PCT, how="upper")
+    df["outflows_norm"] = hd.winsorise(
+        df.outflows_norm, pct=config.WIN_PCT, how="upper"
+    )
+    df["netflows_norm"] = hd.winsorise(
+        df.netflows_norm, pct=config.WIN_PCT / 2, how="both"
+    )
 
-    df["txns_count"] = hd.winsorise(df.txns_count, pct=WIN_PCT, how="upper")
-    df["txns_volume"] = hd.winsorise(df.txns_volume, pct=WIN_PCT, how="upper")
-    df["month_spend"] = hd.winsorise(df.month_spend, pct=WIN_PCT, how="upper")
-    df["discret_spend"] = hd.winsorise(df.discret_spend, pct=WIN_PCT, how="upper")
-    df["month_income"] = hd.winsorise(df.month_income, pct=WIN_PCT, how="upper")
+    df["txns_count"] = hd.winsorise(df.txns_count, pct=config.WIN_PCT, how="upper")
+    df["txns_volume"] = hd.winsorise(df.txns_volume, pct=config.WIN_PCT, how="upper")
+    df["month_spend"] = hd.winsorise(df.month_spend, pct=config.WIN_PCT, how="upper")
+    df["discret_spend"] = hd.winsorise(
+        df.discret_spend, pct=config.WIN_PCT, how="upper"
+    )
+    df["month_income"] = hd.winsorise(df.month_income, pct=config.WIN_PCT, how="upper")
 
     return df
