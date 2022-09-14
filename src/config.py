dcfadb07f163f0d84016627d72ca6e05d7ccd8a9
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-09-14 09:51:04 +0100

    Fix crossrefs

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 AWS_PROFILE = "3di"
 AWS_PIECES = "s3://3di-data-mdb/clean/pieces"
-AWS_PROJECT = "s3://3di-project-eval"
+AWS_PROJECT = "s3://3di-project-entropy"
 
 ROOTDIR = Path(__file__).parent.parent
 FIGDIR = os.path.join(ROOTDIR, "output", "figures")
